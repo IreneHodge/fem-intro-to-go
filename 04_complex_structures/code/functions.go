3b8533841ee3a9eeb84984d47047bd022de5ff1c
Irene Hodge <irenehodge90@gmail.com>
2021-07-13 18:49:30 -0700

    excercise 4a

--- 04_complex_structures/code/functions.go
+++ 04_complex_structures/code/functions.go
@@ -1,15 +1,13 @@
 package main
 
-// import "fmt"
-
-// func printAge(age1, age2 int) (ageOfSally, ageOfBob int) {
-// 	ageOfSally = age1
-// 	ageOfBob = age2
-// 	return
-// }
+func printAge(age1, age2 int) (ageOfSally, ageOfBob int) {
+	ageOfSally = age1
+	ageOfBob = age2
+	return
+}
 
 func main() {
-	// 	x, y := printAge(10, 21)
+		x, y := printAge(10, 21)
 	// 	fmt.Println(x)
 	// 	fmt.Println(y)
 }
---
+++ practice.go
@@ -0,0 +1,17 @@
+//every go file needs a package name
+
+// package main
+
+/*import statement for any packages needed/used. 
+Allow you to access additional libraries that are imported with Go
+*/
+// import "fmt"
+
+/* actual main function-
+entry point for your code
+*/
+
+
+// func main() {
+//     fmt.Println("Hello World!")
+// }

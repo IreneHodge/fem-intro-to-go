dac87a59c89b225c1a2c810ed8a9aac215c8ce3a
Irene Hodge <irenehodge90@gmail.com>
2021-07-13 17:21:50 -0700

    excercise 2a

--- 02_introduction/code/excercise_2a.go
+++ 02_introduction/code/excercise_2a.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
-func main(){
-  fmt.Print("Hello World!")
+// func main(){
+//   fmt.Print("Hello World!")
+// }
+
+func main() {
+	fmt.Printf(`"Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t."`, "Irene", "Sparks, Nevada", 1, true)
 }
